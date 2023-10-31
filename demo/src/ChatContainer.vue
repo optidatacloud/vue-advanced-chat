0912c093f16064843aa65a46b19c6a3ca623bb70
Junior Vitor Ramisch <junior.ramisch@gmail.com>
2023-10-31 18:39:33 -0300

    chore: Provides no-rooms-found-message prop

--- demo/src/ChatContainer.vue
+++ demo/src/ChatContainer.vue
@@ -46,6 +46,7 @@
 			:menu-actions="JSON.stringify(menuActions)"
 			:message-selection-actions="JSON.stringify(messageSelectionActions)"
 			:templates-text="JSON.stringify(templatesText)"
+			:no-rooms-found-message="'No conversation found'"
 			@fetch-more-rooms="fetchMoreRooms"
 			@fetch-messages="fetchMessages($event.detail[0])"
 			@send-message="sendMessage($event.detail[0])"
@@ -81,8 +82,8 @@ import * as storageService from '@/database/storage'
 import { parseTimestamp, formatTimestamp } from '@/utils/dates'
 import logoAvatar from '@/assets/logo.png'
 
-import { register } from 'vue-advanced-chat'
-// import { register } from './../../dist/vue-advanced-chat.es.js'
+// import { register } from 'vue-advanced-chat'
+import { register } from './../../dist/vue-advanced-chat.es.js'
 // import { register } from './../../src/lib/index.js'
 register()
 

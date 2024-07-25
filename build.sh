a31be92d4f880fbb9d50e46de1d215f4d1e329c4
DouglasKosvoski <douglas.contactpro@gmail.com>
2024-07-25 13:28:50 -0300

    fix: undo unwanted changes

--- build.sh
+++
@@ -1,7 +0,0 @@
-#! /usr/bin/sh
-
-clear
-npm run build
-rm -rf ../chat/resources/js/chat/vue-advanced-chat/dist/
-cp -R dist/ ../chat/resources/js/chat/vue-advanced-chat/
-printf "\n\nCompilado em: " && date

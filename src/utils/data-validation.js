a9f5a99b363c5f6e2fceaa3ac6fff3829434be0c
arufonsekun <junior.ramisch@gmail.com>
2024-11-28 17:14:00 -0300

    ui: improves alignment on files-box and change file icons to match Drive

--- src/utils/data-validation.js
+++ src/utils/data-validation.js
@@ -73,3 +73,19 @@ function checkObjectValid(obj, key) {
     obj[key] !== undefined
   )
 }
+
+export function humanFileSize(bytes, si = false, dp = 1) {
+  const thresh = si ? 1000 : 1024
+  const units = si
+    ? ['B', 'kB', 'MB', 'GB', 'TB', 'PB', 'EB', 'ZB', 'YB']
+    : ['B', 'KiB', 'MiB', 'GiB', 'TiB', 'PiB', 'EiB', 'ZiB', 'YiB']
+
+  if (bytes < thresh) {
+    return (bytes || 0) + ' ' + units[0]
+  }
+
+  const u = Math.floor(Math.log(bytes) / Math.log(thresh))
+  const size = (bytes / Math.pow(thresh, u)).toFixed(dp)
+
+  return (size || 0) + ' ' + units[u]
+}

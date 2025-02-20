e0979d38a21610ce844a750e304deb2d3d694efa
DouglasKosvoski <douglas.contactpro@gmail.com>
2025-02-20 18:06:47 -0300

    tweak: icons and shadow

--- src/lib/Room/RoomMessage/MessageFiles/MessageFile/MessageFile.vue
+++ src/lib/Room/RoomMessage/MessageFiles/MessageFile/MessageFile.vue
@@ -52,15 +52,13 @@
               </slot>
             </div>
 
-            <!-- copy file to optiwork drive -->
+            <!-- save file copy into optiwork drive -->
             <div
-              class="vac-svg-button vac-button-copy-to-drive"
+              class="vac-svg-button vac-button-save-copy-into-drive"
               @click.prevent.stop="copyFileToDrive($event)"
             >
-              <slot :name="'document-icon_' + message._id">
-                <svg xmlns="http://www.w3.org/2000/svg" width="17" height="17" viewBox="0 0 24 24" fill="none" stroke="#905da5" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="feather feather-cloud icon-20">
-                  <path d="M18 10h-1.26A8 8 0 1 0 9 20h9a5 5 0 0 0 0-10z" />
-                </svg>
+              <slot :name="'cloud-icon_' + message._id">
+                <svg-icon name="cloud" stroke="royalblue" fill="royalblue" />
               </slot>
             </div>
 
